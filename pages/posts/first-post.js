98b7285d561b95f4a116c9a87877b261332a1719
tomoron <tomoron326@outlook.jp>
2022-02-05 22:09:30 +0900

    add:script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log("script on load")}
+      />
       <h1>First-post</h1>
       <h2>
         <Link href="/">
