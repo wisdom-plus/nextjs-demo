6218452da2e401537ccb463d74cc7ca543d5e99e
tomoron <tomoron326@outlook.jp>
2022-02-07 20:52:29 +0900

    pref:index.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
-import Layout from "../../componets/layout";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
